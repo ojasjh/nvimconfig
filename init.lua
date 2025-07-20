64f5a8ce1dd33622cb1b46dc7e36a95531ef6320
ojas <ojasjha.csv@icloud.com>
2025-07-21 02:49:34 +0530

    first commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+require('config.config')
+require('core.lazy_setup')
+
+
+
+
---
+++ lua/config/background.lua
@@ -0,0 +1,3 @@
+vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
+vim.cmd([[highlight NormalNC guibg=NONE ctermbg=NONE]])
+vim.cmd([[highlight EndOfBuffer guibg=NONE ctermbg=NONE]])
